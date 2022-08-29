a296938f73eabbfc686e52e492c4983438d137fb
ednriduro <endriduro458@gmail.com>
2022-08-29 05:09:29 +0000

    week8

--- LogRocket/20220615-random/hardhat.config.js
+++ LogRocket/20220615-random/hardhat.config.js
@@ -11,33 +11,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
 module.exports = {
   solidity: "0.8.4",
-/*
+
   networks: {
-    "local-devnode": {
-       url: "http://localhost:8545",
-       accounts: { mnemonic: "test test test test test test test test test test test junk" }
-    },
-    "optimistic-kovan": {
-       url: "https://kovan.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
-    },
-    "optimism": {
-       url: "https://mainnet.optimism.io",
-       accounts: { mnemonic: process.env.MNEMONIC }
+    "goerli": {
+       url: "https://opt-goerli.g.alchemy.com/v2/fDRhluE_FrCRRKzNh5ykKdr5NkvmOq9n",
+       accounts: ["38c85db3a121c019f13687bd79f50517795159e8bb0ab405f7d4e643c9aeed40"]
     }
   }
-*/
+
 };
